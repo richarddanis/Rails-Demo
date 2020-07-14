553df615f1a633cc6ba6df820e20b1fd339fbe36
Richard Danis <Richard_Danis@epam.com>
2020-07-14 13:02:03 +0200

    more ignored words

--- app/controllers/api/v1/demo_controller.rb
+++ app/controllers/api/v1/demo_controller.rb
@@ -1,6 +1,7 @@
 require 'faker'
 
 class Api::V1::DemoController < ApplicationController
+    before_action only: [:show, :edit, :update, :destroy]
 
     # GET
     # api/v1/demo
@@ -17,13 +18,16 @@ class Api::V1::DemoController < ApplicationController
                 manufacture: Faker::Vehicle.manufacture
             }
         }
-        render json: first_names.to_json, status: 200
+        # write to console
+        puts first_names 
+        render json: first_names.to_json, status: :ok #or 200
     end
 
     # GET
     # api/v1/demo/:id
     def show
-        id_param = param[:id]
+        puts params[:id]
+        render json: {"param" => params[:id]}
     end
     
     # POST
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
 
   # For details, see https://guides.rubyonrails.org/routing.html 
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # you can defining multiple resource at same time
       # define the id parameter constraints
-      resources :demo, constraints: { id: /[A-Z][A-Z][0-9]+/ }
-      # single resource mapping
+      resources :demo
       # /profile -> return api/v1/demo (GET) result
       get 'profile', to: 'demo#index'
     end
